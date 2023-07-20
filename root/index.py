d69db59ceb217fe0cc6be648aa4ca143b871851c
Ridho Panca Sakti <99238029+ridhopanca@users.noreply.github.com>
2023-07-20 14:28:34 +0700

    allow cors

--- root/index.py
+++ root/index.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from root.model.main import Main
 from root.model.anime import Anime
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def welcome():
