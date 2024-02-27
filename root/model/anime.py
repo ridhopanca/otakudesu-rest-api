52e79afa29a8cae732c16d5cb126e26a3d785ec3
Ridho Panca Sakti <99238029+ridhopanca@users.noreply.github.com>
2024-02-27 10:45:29 +0700

    fix: change  url

--- root/model/anime.py
+++ root/model/anime.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-link = "https://otakudesu.cam/"
+link = "https://otakudesu.cloud/"
 
 class Anime:
     def anime(self, id):
--- root/model/main.py
+++ root/model/main.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-link = "https://otakudesu.cam/"
+link = "https://otakudesu.cloud/"
 replacerId = link+"anime/"
 
 class Main:
